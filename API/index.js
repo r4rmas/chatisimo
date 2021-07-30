01df9a3c632d663326ce9c4ab351fc7e5d789030
Raul Armas <ra@raularmas.com>
2021-07-29 19:08:21 -0500

    ...

--- API/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-require("dotenv/config");
-
-const app = express();
-app.use(express.json());
-
-app.listen(process.env.PORT || 8000, () =>
-  console.log("Listening at http://localhost:8000")
-);
