023b849145b2f4370b2ef456afaf2f768d611ec3
Raul Armas <ra@raularmas.com>
2021-07-31 16:01:04 -0500

    ...

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Description } from "./styles";
+import { Container, Description, Black } from "./styles";
 import { Page } from "../../styles";
 
 const About = () => {
   return (
     <Page>
-      <Description>ABOUT</Description>
+      <Container>
+        <Black>SOBRE NOSOTROS</Black>
+        <Description>
+          Chatisimo es un sistema de chat Open Source que ha sido desarrollado
+          para brindar a sus usuarios una menor carga de datos gracias a su
+          estructura minimalista. Su sistema de protección está basado en no
+          requerir ningún tipo de información ni almacenar cookies.
+        </Description>
+      </Container>
     </Page>
   );
 };
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import colors from "../../colors";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+`;
+
 export const Description = styled.p`
   color: ${colors.black};
   display: flex;
@@ -9,5 +16,14 @@ export const Description = styled.p`
   letter-spacing: 0.1rem;
   line-height: 2.2rem;
   margin-bottom: 2rem;
+  text-align: center;
+`;
+
+export const Black = styled.h2`
+  color: ${colors.black};
+  font-family: "Roboto";
+  font-size: 2rem;
+  font-weight: bolder;
+  margin-bottom: 1.8rem;
   text-align: center;
 `;
